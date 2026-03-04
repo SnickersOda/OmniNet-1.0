e7bcbe1b240350c9cc449faa87d8fc2913a58f42
SnickersOda <hyunixempires@gmail.com>
2026-03-04 16:03:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ HTML = r"""<!DOCTYPE html>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width,initial-scale=1"/>
 <title>OmniumAI</title>
+<link rel="icon" type="image/png" href="/static/favicon.png">
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Syne:wght@600;700;800&family=DM+Sans:wght@300;400;500&family=JetBrains+Mono:wght@400;500&display=swap');
 
